50a39edc173f22dae02caf993262686370174940
ijknabla <ijknabla@gmail.com>
2023-02-03 01:19:25 +0900

    Add python image search

--- omcpyimage/__init__.py
+++ omcpyimage/__init__.py
@@ -1,7 +1,14 @@
-from asyncio import run
+from asyncio import create_subprocess_exec, gather, run
 from collections.abc import Callable, Coroutine
 from functools import wraps
+from itertools import product
+from subprocess import DEVNULL
 from typing import Any, ParamSpec, TypeVar
+
+from numpy import array, bool_
+from numpy.typing import NDArray
+
+from ._types import Debian, Python
 
 P = ParamSpec("P")
 T = TypeVar("T")
@@ -15,3 +22,31 @@ def run_coroutine(
         return run(afunc(*args, **kwargs))
 
     return wrapped
+
+
+async def get_python_vs_debian() -> NDArray[bool_]:
+    flat = array(
+        await gather(
+            *(
+                _exists_in_dockerhub(python=p, debian=d)
+                for p, d in product(Python, Debian)
+            )
+        ),
+        dtype=bool_,
+    )
+    return flat.reshape([len(Python), len(Debian)])
+
+
+async def _exists_in_dockerhub(
+    python: Python,
+    debian: Debian,
+) -> bool:
+    process = await create_subprocess_exec(
+        "docker",
+        "manifest",
+        "inspect",
+        f"python:{python}-{debian}",
+        stdout=DEVNULL,
+    )
+    await process.communicate()
+    return process.returncode == 0
--- omcpyimage/__main__.py
+++ omcpyimage/__main__.py
@@ -1,9 +1,14 @@
-from . import run_coroutine
+from . import get_python_vs_debian, run_coroutine
+from ._types import Debian, Python
 
 
 @run_coroutine
 async def main() -> None:
-    ...
+    if __debug__:
+        python_vs_debian = await get_python_vs_debian()
+        for python, row in zip(Python, python_vs_debian):
+            for debian, value in zip(Debian, row):
+                print(f"{python}-{debian}", value)
 
 
 if __name__ == "__main__":
