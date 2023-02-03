04bdae5e6ab111b224261a9ad108040e9a0c78c8
ijknabla <ijknabla@gmail.com>
2023-02-03 12:04:43 +0900

    Implement tuple property to enum-classes

--- omcpyimage/_types.py
+++ omcpyimage/_types.py
@@ -1,18 +1,40 @@
 import enum
 import re
+from collections.abc import Hashable
 from functools import lru_cache
 from typing import Protocol
 
 
-class SupportsName(Protocol):
+class SupportsName(Hashable, Protocol):
     name: str
 
 
 @lru_cache(None)
 def _enum2version(obj: SupportsName) -> str:
+    return ".".join(map(str, _enum2version_tuple(obj)))
+
+
+@lru_cache(None)
+def _enum2version_tuple(obj: SupportsName) -> tuple[int, int]:
     matched = re.match(r"v(\d+)_(\d+)", obj.name)
     assert matched is not None
-    return ".".join(matched.groups())
+    major, minor = map(int, matched.groups())
+    return major, minor
+
+
+class OpenModelica(enum.Enum):
+    v1_13 = enum.auto()
+    v1_14 = enum.auto()
+    v1_15 = enum.auto()
+    v1_16 = enum.auto()
+    v1_17 = enum.auto()
+    v1_18 = enum.auto()
+    v1_19 = enum.auto()
+    v1_20 = enum.auto()
+
+    @property
+    def tuple(self) -> tuple[int, int]:
+        return _enum2version_tuple(self)
 
 
 class Python(enum.Enum):
