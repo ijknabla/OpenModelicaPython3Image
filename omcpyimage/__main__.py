8cfde834bad15efa8d1f22e26969b7bfbdb99521
ijknabla <ijknabla@gmail.com>
2023-02-03 12:27:13 +0900

    Implement mask calcuration

--- omcpyimage/__main__.py
+++ omcpyimage/__main__.py
@@ -1,3 +1,4 @@
+from asyncio import gather
 from itertools import product
 
 from . import get_openmodelica_vs_debian, get_python_vs_debian, run_coroutine
@@ -6,7 +7,11 @@ from ._types import Debian, OpenModelica, Python
 
 @run_coroutine
 async def main() -> None:
-    openmodelica_vs_debian = await get_openmodelica_vs_debian()
+    openmodelica_vs_debian, python_vs_debian = await gather(
+        get_openmodelica_vs_debian(),
+        get_python_vs_debian(),
+    )
+
     for (i, openmodelica), (j, debian) in product(
         enumerate(OpenModelica), enumerate(Debian)
     ):
@@ -14,10 +19,13 @@ async def main() -> None:
         if 0 <= patch and 0 <= build:
             print(f"{openmodelica}-{debian}", patch, build)
 
-    python_vs_debian = await get_python_vs_debian()
     for python, row in zip(Python, python_vs_debian):
         for debian, value in zip(Debian, row):
             print(f"{python}-{debian}", value)
+
+    xox = (0 <= openmodelica_vs_debian.min(axis=-1))[:, None, :]
+    oxx = python_vs_debian[None, :, :]
+    print(xox & oxx)
 
 
 if __name__ == "__main__":
