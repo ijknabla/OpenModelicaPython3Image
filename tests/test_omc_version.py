1afce5ed89f29b94d964cd0607b1dbc171f44e2d
ijknabla <ijknabla@gmail.com>
2023-02-04 19:57:33 +0900

    Implement test_omc_version

---
+++ tests/test_omc_version.py
@@ -0,0 +1,21 @@
+import pytest
+
+from omcpyimage._types import Level, VersionTuple
+
+
+@pytest.mark.parametrize("build", range(2))
+@pytest.mark.parametrize("stage", [None, *range(2)])
+@pytest.mark.parametrize("release", Level)
+@pytest.mark.parametrize("micro", range(2))
+@pytest.mark.parametrize("minor", range(2))
+@pytest.mark.parametrize("major", range(2))
+def test_omc_version(
+    major: int,
+    minor: int,
+    micro: int,
+    release: Level,
+    stage: int | None,
+    build: int,
+) -> None:
+    version = VersionTuple(major=major, minor=minor, micro=micro)
+    print(version.omc_repr)
