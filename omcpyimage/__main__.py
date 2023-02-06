8a7d6c68fae749316f4c105ed10bc7464194940e
ijknabla <ijknabla@gmail.com>
2023-02-06 12:10:29 +0900

    Add config.toml reading

--- omcpyimage/__main__.py
+++ omcpyimage/__main__.py
@@ -1,14 +1,24 @@
 from asyncio import gather
+from typing import IO
+
+import click
+import toml
 
 from . import get_openmodelica_vs_distro, get_python_vs_debian, run_coroutine
+from ._apis import is_setting
 from ._types import Debian, DistroName, Python
 
 
+@click.command
+@click.argument("config_io", metavar="CONFIG.TOML", type=click.File("r"))
 @run_coroutine
-async def main() -> None:
-    openmodelica_vs_distro = await get_openmodelica_vs_distro(
-        map(DistroName, ["stretch", "buster", "bullseye"])
-    )
+async def main(
+    config_io: IO[str],
+) -> None:
+    config = toml.load(config_io)
+    assert is_setting(config)
+
+    openmodelica_vs_distro = await get_openmodelica_vs_distro(config["distro"])
 
     for (distro_name, _), omc_version in openmodelica_vs_distro.items():
         print(f"{distro_name=!s}, {omc_version=!s}")
