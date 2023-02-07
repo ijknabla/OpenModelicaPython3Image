927b371a9ba07d8e527c7318d9ad5df8e2a0e796
ijknabla <ijknabla@gmail.com>
2023-02-07 16:05:12 +0900

    Add Version & LongVersion

--- omcpyimage/_types.py
+++ omcpyimage/_types.py
@@ -1,8 +1,47 @@
 import enum
 import re
-from collections.abc import Hashable
+from collections.abc import Hashable, Iterator
+from dataclasses import dataclass
 from functools import lru_cache, total_ordering
-from typing import Protocol
+from typing import ClassVar, Protocol
+
+
+@dataclass(frozen=True, order=True)
+class Version:
+    major: int
+    minor: int
+
+    __pattern__: ClassVar[re.Pattern[str]] = re.compile(
+        r"^(?P<major>\d+)\.(?P<minor>\d+)$"
+    )
+
+    def __iter__(self) -> Iterator[int]:
+        yield self.major
+        yield self.minor
+
+    def __str__(self) -> str:
+        return ".".join(map(str, self))
+
+    @classmethod
+    def parse(cls, s: str) -> "Version":
+        match = cls.__pattern__.match(s)
+        if match is None:
+            raise ValueError(f"{s!r} does not match {cls.__pattern__.pattern}")
+        return cls(*map(int, match.groups()))
+
+
+@dataclass(frozen=True, order=True)
+class LongVersion(Version):
+    micro: int
+
+    __pattern__ = re.compile(
+        r"^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<micro>\d+)$"
+    )
+
+    def __iter__(self) -> Iterator[int]:
+        yield self.major
+        yield self.minor
+        yield self.micro
 
 
 class SupportsName(Hashable, Protocol):
