dbc0be5b397e48944d1785c832711ec27b48581b
ijknabla <ijknabla@gmail.com>
2023-11-24 20:58:38 +0900

    Update property

--- omcpyimage/config.py
+++ omcpyimage/config.py
@@ -4,31 +4,61 @@ from typing import Annotated, NamedTuple
 from pydantic import BaseModel, PlainSerializer, PlainValidator
 
 
-class Version(NamedTuple):
+class ShortVersion(NamedTuple):
     major: int
     minor: int
 
 
 @PlainValidator
-def _validate_version(v: Version | str) -> Version:
-    if isinstance(v, Version):
+def _validate_short_version(v: ShortVersion | str) -> ShortVersion:
+    if isinstance(v, ShortVersion):
         return v
     if isinstance(v, str):
         if (matched := re.match(r"(\d+)\.(\d+)", v)) is None:
             raise ValueError(v)
-        return Version(*map(int, matched.groups()))
+        return ShortVersion(*map(int, matched.groups()))
 
     raise ValueError(v)
 
 
 @PlainSerializer
-def _serialize_version(v: Version) -> str:
+def _serialize_short_version(v: ShortVersion) -> str:
     return f"{v.major}.{v.minor}"
 
 
-AnnotatedVersion = Annotated[Version, _validate_version, _serialize_version]
+AnnotatedShortVersion = Annotated[
+    ShortVersion, _validate_short_version, _serialize_short_version
+]
+
+
+class LongVersion(NamedTuple):
+    major: int
+    minor: int
+    patch: int
+
+
+@PlainValidator
+def _validate_long_version(v: LongVersion | str) -> LongVersion:
+    if isinstance(v, LongVersion):
+        return v
+    if isinstance(v, str):
+        if (matched := re.match(r"(\d+)\.(\d+)\.(\d+)", v)) is None:
+            raise ValueError(v)
+        return LongVersion(*map(int, matched.groups()))
+
+    raise ValueError(v)
+
+
+@PlainSerializer
+def _serialize_long_version(v: ShortVersion) -> str:
+    return f"{v.major}.{v.minor}"
+
+
+AnnotatedLongVersion = Annotated[
+    LongVersion, _validate_long_version, _serialize_long_version
+]
 
 
 class Config(BaseModel):
-    omc: list[AnnotatedVersion]
-    py: list[AnnotatedVersion]
+    openmodelica: list[AnnotatedLongVersion]
+    python: list[AnnotatedShortVersion]
