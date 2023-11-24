0803325be069a52e27e6d0b636a6598bfeefb718
ijknabla <ijknabla@gmail.com>
2023-11-24 17:19:05 +0900

    Implement Version type

--- omcpyimage/config.py
+++ omcpyimage/config.py
@@ -1,6 +1,34 @@
-from pydantic import BaseModel
+import re
+from typing import Annotated, NamedTuple
+
+from pydantic import BaseModel, PlainSerializer, PlainValidator
+
+
+class Version(NamedTuple):
+    major: int
+    minor: int
+
+
+@PlainValidator
+def _validate_version(v: Version | str) -> Version:
+    if isinstance(v, Version):
+        return v
+    if isinstance(v, str):
+        if (matched := re.match(r"(\d+)\.(\d+)", v)) is None:
+            raise ValueError(v)
+        return Version(*map(int, matched.groups()))
+
+    raise ValueError(v)
+
+
+@PlainSerializer
+def _serialize_version(v: Version) -> str:
+    return f"{v.major}.{v.minor}"
+
+
+AnnotatedVersion = Annotated[Version, _validate_version, _serialize_version]
 
 
 class Config(BaseModel):
-    omc: list[str]
-    py: list[str]
+    omc: list[AnnotatedVersion]
+    py: list[AnnotatedVersion]
