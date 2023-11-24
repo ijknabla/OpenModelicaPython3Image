5283741a892acd309ec1d4179dc5d8750c7eabc1
ijknabla <ijknabla@gmail.com>
2023-11-24 17:07:51 +0900

    Update config check

--- omcpyimage/__main__.py
+++ omcpyimage/__main__.py
@@ -1,27 +1,25 @@
 from asyncio import Semaphore
-from pathlib import Path
+from typing import IO
 
 import click
 import toml
 
 from . import ImageBuilder, run_coroutine
 from ._api import is_config, sort_cache
+from .config import Config
 
 
 @click.command
 @click.argument(
-    "config_path",
+    "config_io",
     metavar="CONFIG.TOML",
-    type=click.Path(
-        exists=True, file_okay=True, dir_okay=False, path_type=Path
-    ),
+    type=click.File(mode="r", encoding="utf-8"),
 )
 @click.option("--limit", type=int)
 @run_coroutine
-async def main(config_path: Path, limit: int | None) -> None:
+async def main(config_io: IO[str], limit: int | None) -> None:
+    config = Config.model_validate(toml.load(config_io))
     return
-    config = toml.loads(config_path.read_text(encoding="utf-8"))
-    assert is_config(config)
 
     try:
         if limit is not None:
@@ -32,7 +30,6 @@ async def main(config_path: Path, limit: int | None) -> None:
     finally:
         assert is_config(config)
         sort_cache(config)
-        config_path.write_text(toml.dumps(config), encoding="utf-8")
 
 
 if __name__ == "__main__":
