1ed451223115993ad5813ff904c211760058de1a
ijknabla <ijknabla@gmail.com>
2023-11-24 17:09:30 +0900

    Remove _api import

--- omcpyimage/__main__.py
+++ omcpyimage/__main__.py
@@ -5,7 +5,6 @@ import click
 import toml
 
 from . import ImageBuilder, run_coroutine
-from ._api import is_config, sort_cache
 from .config import Config
 
 
@@ -21,15 +20,11 @@ async def main(config_io: IO[str], limit: int | None) -> None:
     config = Config.model_validate(toml.load(config_io))
     return
 
-    try:
-        if limit is not None:
-            lock = Semaphore(limit)
-        else:
-            lock = None
-        await ImageBuilder(config).build(lock)
-    finally:
-        assert is_config(config)
-        sort_cache(config)
+    if limit is not None:
+        lock = Semaphore(limit)
+    else:
+        lock = None
+    await ImageBuilder(config).build(lock)
 
 
 if __name__ == "__main__":
