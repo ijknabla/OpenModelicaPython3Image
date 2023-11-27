a558d3348368f7fc2e218ad37480bc3476d0e364
ijknabla <ijknabla@gmail.com>
2023-11-27 13:43:32 +0900

    Update ShortVersion

--- omcpyimage/config.py
+++ omcpyimage/config.py
@@ -1,32 +1,23 @@
-import re
-from typing import Annotated, NamedTuple
+from typing import Annotated
 
 from pydantic import BaseModel, Field, PlainSerializer, PlainValidator
 
-
-class ShortVersion(NamedTuple):
-    major: int
-    minor: int
-
-    def __str__(self) -> str:
-        return f"{self.major}.{self.minor}"
+from .types import ShortVersion
 
 
 @PlainValidator
 def _validate_short_version(v: ShortVersion | str) -> ShortVersion:
     if isinstance(v, ShortVersion):
         return v
-    if isinstance(v, str):
-        if (matched := re.match(r"(\d+)\.(\d+)", v)) is None:
-            raise ValueError(v)
-        return ShortVersion(*map(int, matched.groups()))
-
-    raise ValueError(v)
+    elif isinstance(v, str):
+        return ShortVersion.parse(v)
+    else:
+        raise ValueError(v)
 
 
 @PlainSerializer
 def _serialize_short_version(v: ShortVersion) -> str:
-    return f"{v.major}.{v.minor}"
+    return str(v)
 
 
 AnnotatedShortVersion = Annotated[
--- omcpyimage/types.py
+++ omcpyimage/types.py
@@ -1,7 +1,19 @@
 import re
 from typing import NamedTuple, Self
 
-from .config import ShortVersion
+
+class ShortVersion(NamedTuple):
+    major: int
+    minor: int
+
+    @classmethod
+    def parse(cls, s: str) -> Self:
+        if (matched := re.search(r"(\d+)\.(\d+)", s)) is None:
+            raise ValueError(s)
+        return cls(*map(int, matched.groups()))
+
+    def __str__(self) -> str:
+        return f"{self.major}.{self.minor}"
 
 
 class LongVersion(NamedTuple):
