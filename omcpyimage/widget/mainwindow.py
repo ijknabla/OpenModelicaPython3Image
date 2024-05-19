f3e3e888fdaba61302f241bf25cad9def9e271b8
ijknabla <ijknabla@gmail.com>
2024-05-19 21:54:56 +0900

    Add MainWindow

---
+++ omcpyimage/widget/mainwindow.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import QMainWindow, QWidget
+
+
+class MainWindow(QMainWindow):
+    def __init__(
+        self,
+        parent: QWidget | None = None,
+    ) -> None:
+        super().__init__(parent)
