8feccf2beb18cadadcb9660b1f5df777c3081b41
ijknabla <ijknabla@gmail.com>
2024-05-21 01:53:25 +0900

    Implement in_executor

--- omcpyimage/util.py
+++ omcpyimage/util.py
@@ -1,10 +1,23 @@
 from __future__ import annotations
 
+from asyncio import Future, get_running_loop
 from asyncio.subprocess import Process
-from collections.abc import AsyncGenerator, Iterator
+from collections.abc import AsyncGenerator, Callable, Iterator
 from contextlib import asynccontextmanager, contextmanager, suppress
+from functools import partial, wraps
 from subprocess import Popen
-from typing import AnyStr
+from typing import AnyStr, ParamSpec, TypeVar
+
+P = ParamSpec("P")
+T = TypeVar("T")
+
+
+def in_executor(f: Callable[P, T]) -> Callable[P, Future[T]]:
+    @wraps(f)
+    def wrapped(*args: P.args, **kwargs: P.kwargs) -> Future[T]:
+        return get_running_loop().run_in_executor(None, partial(f, *args, **kwargs))
+
+    return wrapped
 
 
 @contextmanager
