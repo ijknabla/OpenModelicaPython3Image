fe0f96be2375cf865d250bb8c52ffd3b79f45e0d
ijknabla <ijknabla@gmail.com>
2024-05-21 01:34:23 +0900

    Implement terminating

--- omcpyimage/util.py
+++ omcpyimage/util.py
@@ -1,8 +1,22 @@
 from __future__ import annotations
 
 from asyncio.subprocess import Process
-from collections.abc import AsyncGenerator
-from contextlib import asynccontextmanager, suppress
+from collections.abc import AsyncGenerator, Iterator
+from contextlib import asynccontextmanager, contextmanager, suppress
+from subprocess import Popen
+from typing import AnyStr
+
+
+@contextmanager
+def terminating(
+    process: Popen[AnyStr],
+) -> Iterator[Popen[AnyStr]]:
+    try:
+        yield process
+    finally:
+        if process.poll() is None:
+            process.terminate()
+            process.wait()
 
 
 @asynccontextmanager
