0f43fcc49045ff4da36972150610a7a8c9071247
ijknabla <ijknabla@gmail.com>
2024-10-16 14:04:14 +0900

    Implement model utilities

---
+++ omcpyimage/model/__init__.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from asyncio import run
+from collections.abc import Awaitable, Callable
+from concurrent.futures import Executor, Future
+from functools import partial, wraps
+from typing import Concatenate, ParamSpec, Protocol, TypeVar
+
+P = ParamSpec("P")
+T = TypeVar("T")
+AnySupportsExecutor = TypeVar("AnySupportsExecutor", bound="SupportsExecutor")
+
+
+def run_in_executor(
+    f: Callable[Concatenate[AnySupportsExecutor, P], Awaitable[T]],
+) -> Callable[Concatenate[AnySupportsExecutor, P], T]:
+    @wraps(f)
+    def g(self: AnySupportsExecutor, /, *args: P.args, **kwargs: P.kwargs) -> T:
+        future: Future[T] = self.executor.submit(partial(run, f(self, *args, **kwargs)))
+        return future.result()
+
+    return g
+
+
+class SupportsExecutor(Protocol):
+    @property
+    def executor(self) -> Executor: ...
