58c2729930ef4c7f3b9f78f29e6b78e7d8c50bc2
ijknabla <ijknabla@gmail.com>
2024-10-16 15:57:46 +0900

    Add ui

--- omcpyimage/widget/mainwindow.py
+++ omcpyimage/widget/mainwindow.py
@@ -1,4 +1,13 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QMainWindow, QWidget
+
+from ..ui.mainwindow import Ui_MainWindow
 
 
-class MainWindow(QMainWindow): ...
+class MainWindow(QMainWindow):
+    def __init__(
+        self, parent: QWidget | None = None, flags: Qt.WindowType | None = None
+    ) -> None:
+        super().__init__(parent, flags if flags is not None else Qt.WindowType(0))
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)  # type: ignore [no-untyped-call]
