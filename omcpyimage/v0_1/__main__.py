081b5abb6fe3fac9910bc2be6e8f1049f49e61e4
ijknabla <ijknabla@gmail.com>
2024-10-21 11:57:21 +0900

    Add docker image

--- omcpyimage/v0_1/__main__.py
+++ omcpyimage/v0_1/__main__.py
@@ -1,8 +1,13 @@
+from importlib.resources import as_file, files
+from subprocess import run
+
 import click
 
 
 @click.command()
-def main() -> None: ...
+def main() -> None:
+    with as_file(files(__package__)) as directory:
+        run(["docker", "build", f"{directory}"], check=True)
 
 
 if __name__ == "__main__":
