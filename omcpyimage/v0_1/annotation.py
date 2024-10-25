864adf14e47dad9c73068d49531093ec12a8cfda
ijknabla <ijknabla@gmail.com>
2024-10-26 00:03:01 +0900

    Implement annotation

---
+++ omcpyimage/v0_1/annotation.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+from typing import TYPE_CHECKING, Any, Generic, TypeVar
+
+import PySide6.QtCore
+from PySide6.QtCore import QMetaObject, QObject, Qt
+
+_T = TypeVar("_T")
+
+
+if TYPE_CHECKING:
+
+    class Signal(Generic[_T]):
+        def __init__(
+            self,
+            type: type[_T],
+            *,
+            name: str | None = ...,
+            arguments: list[str] | None = ...,
+        ) -> None: ...
+
+        def __get__(
+            self, obj: QObject, objtype: type[QObject] | None = None, /
+        ) -> SignalInstance[_T]: ...
+
+    class SignalInstance(Generic[_T]):
+        def connect(
+            self, slot: Callable[[_T], Any], type: Qt.ConnectionType = ...
+        ) -> QMetaObject.Connection: ...
+        def disconnect(self, slot: Callable[[_T], Any] | None = ...) -> bool: ...
+        def emit(self, arg: _T, /) -> None: ...
+
+else:
+    Signal = PySide6.QtCore.Signal
+
+    class SignalInstance:
+        def __class_getitem__(self, _, /):
+            return PySide6.QtCore.SignalInstance
