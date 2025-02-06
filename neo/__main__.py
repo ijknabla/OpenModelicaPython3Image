a3241025c0e7017aba2ceedaaa134e3c8e080088
ijknabla <ijknabla@gmail.com>
2025-02-06 23:23:21 +0900

    Add first image sample

--- neo/__main__.py
+++ neo/__main__.py
@@ -12,9 +12,21 @@ from . import DockerBake
 @(lambda f: wraps(f)(lambda *args, **kwargs: run(f(*args, **kwargs))))
 async def main(*, indent: int | None) -> None:
     sys.exit(
-        await DockerBake.model_validate({"group": {"default": {"targets": []}}}).build(
-            indent=indent
-        )
+        await DockerBake.model_validate(
+            {
+                "group": {"default": {"targets": ["1_24_0-3_12_7"]}},
+                "target": {
+                    "1_24_0-3_12_7": {
+                        "context": ".",
+                        "dockerfile": "Dockerfile",
+                        "args": {"BASE_IMAGE": "ubuntu:20.04"},
+                        "tags": [
+                            # "myapp:ubuntu", "myapp:latest"
+                        ],
+                    },
+                },
+            }
+        ).build(indent=indent)
     )
 
 
