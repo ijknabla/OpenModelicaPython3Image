ce6f45836bfed6e4ac47bbf0fe7cb499afe33720
ijknabla <ijknabla@gmail.com>
2025-02-06 23:47:50 +0900

    Add Target class

--- neo/__init__.py
+++ neo/__init__.py
@@ -1,5 +1,9 @@
+from __future__ import annotations
+
 from asyncio import subprocess
+from collections.abc import Sequence
 from importlib.resources import as_file, files
+from typing import NewType, Self
 
 from pydantic import BaseModel, ConfigDict
 
@@ -17,3 +21,52 @@ class DockerBake(BaseModel):
                 "docker", "buildx", "bake", cwd=package_directory
             )
             return await process.wait()
+
+    @classmethod
+    def from_targets(cls, targets: Sequence[Target]) -> Self:
+        return cls.model_validate(
+            {
+                "group": {"default": {"targets": [x.name for x in targets]}},
+                "target": {
+                    x.name: {
+                        "context": ".",
+                        "dockerfile": "Dockerfile",
+                        "args": x.args,
+                        "tags": [
+                            # "myapp:ubuntu", "myapp:latest"
+                        ],
+                    }
+                    for x in targets
+                },
+            }
+        )
+
+
+_TargetName = NewType("_TargetName", str)
+
+
+class Target(BaseModel):
+    openmodelica: tuple[int, int, int]
+    python: tuple[int, int, int]
+
+    @property
+    def name(self) -> _TargetName:
+        return _TargetName(
+            "{}-{}".format(
+                "_".join(map(str, self.openmodelica)),
+                "_".join(map(str, self.python)),
+            )
+        )
+
+    @property
+    def args(self) -> dict[str, str]:
+        OM_MAJOR, OM_MINOR, OM_PATCH = map(str, self.openmodelica)
+        PY_MAJOR, PY_MINOR, PY_PATCH = map(str, self.python)
+        return dict(
+            OM_MAJOR=OM_MAJOR,
+            OM_MINOR=OM_MINOR,
+            OM_PATCH=OM_PATCH,
+            PY_MAJOR=PY_MAJOR,
+            PY_MINOR=PY_MINOR,
+            PY_PATCH=PY_PATCH,
+        )
--- neo/__main__.py
+++ neo/__main__.py
@@ -4,30 +4,21 @@ from functools import wraps
 
 import click
 
-from . import DockerBake
+from . import DockerBake, Target
 
 
 @click.command()
 @click.option("--indent", type=int)
 @(lambda f: wraps(f)(lambda *args, **kwargs: run(f(*args, **kwargs))))
 async def main(*, indent: int | None) -> None:
-    sys.exit(
-        await DockerBake.model_validate(
-            {
-                "group": {"default": {"targets": ["1_24_0-3_12_7"]}},
-                "target": {
-                    "1_24_0-3_12_7": {
-                        "context": ".",
-                        "dockerfile": "Dockerfile",
-                        "args": {"BASE_IMAGE": "ubuntu:20.04"},
-                        "tags": [
-                            # "myapp:ubuntu", "myapp:latest"
-                        ],
-                    },
-                },
-            }
-        ).build(indent=indent)
-    )
+    targets = [
+        Target(
+            openmodelica=(1, 24, 0),
+            python=(3, 12, 7),
+        )
+    ]
+
+    sys.exit(await DockerBake.from_targets(targets).build(indent=indent))
 
 
 if __name__ == "__main__":
