e230bb1af7c855aa7c38249644baed8fecbf4b96
ijknabla <ijknabla@gmail.com>
2025-02-07 00:10:03 +0900

    Implement iter_tag_in_remote

--- neo/__init__.py
+++ neo/__init__.py
@@ -1,11 +1,30 @@
 from __future__ import annotations
 
+import re
 from asyncio import subprocess
-from collections.abc import Sequence
+from collections.abc import AsyncIterator, Sequence
 from importlib.resources import as_file, files
 from typing import NewType, Self
 
 from pydantic import BaseModel, ConfigDict
+
+_URI = NewType("_URI", str)
+_Tag = NewType("_Tag", str)
+_TargetName = NewType("_TargetName", str)
+
+OPENMODELICA_URI = "https://github.com/OpenModelica/OpenModelica.git"
+PYTHON_URI = "https://github.com/python/cpython.git"
+
+
+async def iter_tags_in_remote(uri: _URI) -> AsyncIterator[_Tag]:
+    process = await subprocess.create_subprocess_exec(
+        "git", "ls-remote", "--tags", uri, stdout=subprocess.PIPE
+    )
+    if process.stdout is None:
+        raise RuntimeError
+    async for buffer in process.stdout:
+        for matched in re.finditer(rb"[a-z0-9]{40}\s+(?P<tag>\S+)", buffer):
+            yield _Tag(matched.group("tag").decode("ascii"))
 
 
 class DockerBake(BaseModel):
@@ -40,9 +59,6 @@ class DockerBake(BaseModel):
                 },
             }
         )
-
-
-_TargetName = NewType("_TargetName", str)
 
 
 class Target(BaseModel):
